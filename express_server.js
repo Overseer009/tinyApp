b0822ab130d20af2c584d74eee18617c7bc6c6f6
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-11 16:31:30 -0400

    completed the route for log out and it clears the cookie

--- express_server.js
+++ express_server.js
@@ -47,7 +47,13 @@ app.post("/login", (req, res) => {
   const username = req.body.username
   res.cookie("username", username)
   res.redirect('/urls')
-})
+});
+
+//allows the user to log out
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  res.redirect('/urls')
+});
 
 //GETs-----------------------------------------------------------
 
