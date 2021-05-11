b32580b7b08ffb6a1760cf21ca58ba3b5cac128f
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-11 15:37:33 -0400

    the cookie is now created when the username is logged in

--- express_server.js
+++ express_server.js
@@ -40,6 +40,13 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect('/urls');
 });
 
+//allows for user to login
+app.post("/login", (req, res) => {
+  const username = req.body.username
+  res.cookie("username", username)
+  res.redirect('/urls')
+})
+
 //GETs-----------------------------------------------------------
 
 app.get("/urls", (req, res) => {
