d21bf20978d4c7f50db8321e4e041cbb22e8bfd9
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-11 11:27:07 -0400

    added the route to the edit button

--- express_server.js
+++ express_server.js
@@ -32,28 +32,24 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//Leads to the main site of our longURL through our short URL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
 
+//Route that deletes a URL from the user's list of shortened URLs.
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+//
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+//Generates a six character alpha-numeric string that is out shortURLs
 const generateRandomString = function() {
   const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let ranStr = '';
